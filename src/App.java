940e35effe3d61bb053a507c2bcd51fb288b9e27
wilberth <wgcampos15@gmail.com>
2022-09-18 21:53:47 -0500

    class inutil eliminada

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
         LectorArchivo lectorArchivo = new LectorArchivo();
         lectorArchivo.leerArchivo();
     }
